1bca83f77d1765871b102231cc5077f53778edb8
Bisury1 <fnebaev@mail.ru>
2022-07-29 13:56:09 +0300

    first commit serialize

---
+++ Serialization/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Serialization
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TypeSelector = new System.Windows.Forms.ComboBox();
+            this.ForPrintDeserializedInfo = new System.Windows.Forms.ListBox();
+            this.SerializedButton = new System.Windows.Forms.Button();
+            this.DeserializedButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TypeSelector
+            // 
+            this.TypeSelector.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.TypeSelector.FormattingEnabled = true;
+            this.TypeSelector.Items.AddRange(new object[] {
+            "Binary",
+            "JSON",
+            "XML"});
+            this.TypeSelector.Location = new System.Drawing.Point(584, 70);
+            this.TypeSelector.Name = "TypeSelector";
+            this.TypeSelector.Size = new System.Drawing.Size(147, 37);
+            this.TypeSelector.TabIndex = 0;
+            // 
+            // ForPrintDeserializedInfo
+            // 
+            this.ForPrintDeserializedInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.ForPrintDeserializedInfo.FormattingEnabled = true;
+            this.ForPrintDeserializedInfo.ItemHeight = 20;
+            this.ForPrintDeserializedInfo.Location = new System.Drawing.Point(12, 44);
+            this.ForPrintDeserializedInfo.Name = "ForPrintDeserializedInfo";
+            this.ForPrintDeserializedInfo.Size = new System.Drawing.Size(539, 364);
+            this.ForPrintDeserializedInfo.TabIndex = 1;
+            // 
+            // SerializedButton
+            // 
+            this.SerializedButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.SerializedButton.Location = new System.Drawing.Point(584, 232);
+            this.SerializedButton.Name = "SerializedButton";
+            this.SerializedButton.Size = new System.Drawing.Size(147, 37);
+            this.SerializedButton.TabIndex = 2;
+            this.SerializedButton.Text = "Serialized";
+            this.SerializedButton.UseVisualStyleBackColor = true;
+            this.SerializedButton.Click += new System.EventHandler(this.SerializedButton_Click);
+            // 
+            // DeserializedButton
+            // 
+            this.DeserializedButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.DeserializedButton.Location = new System.Drawing.Point(576, 342);
+            this.DeserializedButton.Name = "DeserializedButton";
+            this.DeserializedButton.Size = new System.Drawing.Size(175, 37);
+            this.DeserializedButton.TabIndex = 3;
+            this.DeserializedButton.Text = "Deserialized";
+            this.DeserializedButton.UseVisualStyleBackColor = true;
+            this.DeserializedButton.Click += new System.EventHandler(this.DeserializedButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DeserializedButton);
+            this.Controls.Add(this.SerializedButton);
+            this.Controls.Add(this.ForPrintDeserializedInfo);
+            this.Controls.Add(this.TypeSelector);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox TypeSelector;
+        private System.Windows.Forms.ListBox ForPrintDeserializedInfo;
+        private System.Windows.Forms.Button SerializedButton;
+        private System.Windows.Forms.Button DeserializedButton;
+    }
+}
+
---
+++ Serialization/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+    #region FormInterface
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SerializedButton_Click(object sender, EventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+            ForPrintDeserializedInfo.Items.Clear();
+            try
+            {
+                switch (TypeSelector.SelectedItem.ToString())
+                {
+                    case "Binary":
+                        if (fbd.ShowDialog() == DialogResult.OK)
+                        {
+                            new BaseSerialization(fbd.SelectedPath, (stream, informs) => new BinaryFormatter().Serialize(stream, informs))
+                                .Serialization($"result{DateTime.Now.Day}.bin");
+                        }
+                        break;
+                    case "XML":
+                        if (fbd.ShowDialog() == DialogResult.OK)
+                        {
+                            new BaseSerialization(fbd.SelectedPath, (stream, informs) =>
+                                    new XmlSerializer(typeof(List<Inform>)).Serialize(stream, informs))
+                                .Serialization($"result{DateTime.Now.Day}.xml");
+                        }
+                        break;
+                    case "JSON":
+                        if (fbd.ShowDialog() == DialogResult.OK)
+                        {
+                            new BaseSerialization(fbd.SelectedPath, (stream, informs) => JsonSerializer.Serialize(stream, informs))
+                                .Serialization($"result{DateTime.Now.Day}.json");
+                        }
+                        break;
+                }
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("You need to select the type of serialization!");
+            }
+            catch
+            {
+                MessageBox.Show("Something was wrong");
+            }
+        }
+
+        private void DeserializedButton_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ForPrintDeserializedInfo.Items.Clear();
+            try
+            {
+                switch (TypeSelector.SelectedItem.ToString())
+                {
+                    case "Binary":
+                        if (ofd.ShowDialog() == DialogResult.OK)
+                        {
+                            foreach (var item in new BaseDeserialization(stream => (List<Inform>)new BinaryFormatter().Deserialize(stream))
+                                         .Deserialization(ofd.FileName))
+                            {
+                                ForPrintDeserializedInfo.Items.Add(item);
+                            }
+                        }
+                        break;
+                    case "XML":
+                        if (ofd.ShowDialog() == DialogResult.OK)
+                        {
+                            foreach (var item in new BaseDeserialization(stream => 
+                                             (List<Inform>)new XmlSerializer(typeof(List<Inform>)).Deserialize(stream))
+                                         .Deserialization(ofd.FileName))
+                            {
+                                ForPrintDeserializedInfo.Items.Add(item);
+                            }
+                        }
+                        break;
+                    case "JSON":
+                        if (ofd.ShowDialog() == DialogResult.OK)
+                        {
+                            foreach (var item in new BaseDeserialization(stream => JsonSerializer.Deserialize<List<Inform>>(stream))
+                                         .Deserialization(ofd.FileName))
+                            {
+                                ForPrintDeserializedInfo.Items.Add(item);
+                            }
+                        }
+                        break;
+                }
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("You need to select the type of deserialization!");
+            }
+            catch
+            {
+                MessageBox.Show("Something was wrong");
+            }
+        }
+    }
+    #endregion
+
+    #region Deserialization Class
+
+    public class BaseDeserialization
+    {
+        private readonly Func<Stream, List<Inform>> _deserializationFunc;
+
+        public BaseDeserialization(Func<Stream, List<Inform>> deserializationFunc)
+        {
+            _deserializationFunc = deserializationFunc;
+        }
+        public List<Inform> Deserialization(string nameOfTheDeserializedFile)
+        {
+            using (Stream fs = new FileStream(nameOfTheDeserializedFile, FileMode.Open, FileAccess.Read))
+            {
+                return _deserializationFunc?.Invoke(fs);
+            }
+        }
+    }
+    #endregion
+
+    #region Serialization Class
+    public class BaseSerialization
+    {
+        private readonly DirectoryAndFileHandler _directoryAndFileHandler;
+        private readonly Action<Stream, List<Inform>> _serializeFunction;
+
+        public BaseSerialization(string path, Action<Stream, List<Inform>> serializeFunction)
+        {
+            _directoryAndFileHandler = new DirectoryAndFileHandler(path);
+            _serializeFunction = serializeFunction;
+        }
+
+        public void Serialization(string nameOfTheSerializedFile)
+        {
+            using (Stream fs = new FileStream(nameOfTheSerializedFile, FileMode.Create, FileAccess.Write))
+            {
+                _serializeFunction?.Invoke(fs, _directoryAndFileHandler.DirectoryProcessing());
+            }
+        }
+    }
+    #endregion
+
+    #region Serializable Class
+    [Serializable]
+    public class Inform
+    {
+        public string Name { get; set; }
+        public long Size { get; set; }
+        public DateTime ChangeTime { get; set; }
+        public FileAttributes Attributes { get; set; }
+        public bool IsDirectory { get; set; }
+
+        public Inform()
+        {
+        }
+        public Inform(FileInfo file)
+        {
+            Name = file.Name;
+            Size = file.Length;
+            ChangeTime = file.LastWriteTime;
+            Attributes = file.Attributes;
+        }
+        public Inform(DirectoryInfo file)
+        {
+            Name = file.Name;
+            Size = GetWshFolderSize(file.FullName);
+            ChangeTime = file.LastWriteTime;
+            Attributes = file.Attributes;
+            IsDirectory = true;
+        }
+        public long GetWshFolderSize(string fldr)
+        {
+            var fso = new IWshRuntimeLibrary.FileSystemObject();
+            var fldrSize = (long)fso.GetFolder(fldr).Size;
+            Marshal.FinalReleaseComObject(fso);
+            return fldrSize;
+        }
+
+        public override string ToString()
+        {
+            return string.Format(
+                $"Name: {Name}, Size: {Size}, Change Time: {ChangeTime}, Attributes: {Attributes}, Is Directory: {IsDirectory}");
+        }
+    }
+    #endregion
+
+    #region DirectoryHandler
+    public class DirectoryAndFileHandler
+    {
+        private readonly DirectoryInfo _directory;
+        public DirectoryAndFileHandler(string path)
+        {
+            _directory = new DirectoryInfo(path);
+        }
+
+        public List<Inform> DirectoryProcessing()
+        {
+            var dirs = new Stack<DirectoryInfo>();
+            dirs.Push(_directory);
+            var listForFileInform = new List<Inform>();
+            while (dirs.Count > 0)
+            {
+                var currentDir = dirs.Pop();
+                listForFileInform.Add(new Inform(currentDir));
+                var subDirs = Array.Empty<DirectoryInfo>();
+                try
+                {
+                    subDirs = currentDir.GetDirectories();
+                    listForFileInform.AddRange(currentDir.GetFiles().Select(file => new Inform(file)));
+                    foreach (var str in subDirs)
+                        dirs.Push(str);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    MessageBox.Show("You don't have discovery permossion on a folder or file");
+                }
+                catch
+                {
+                    MessageBox.Show("Sonething was wrong. I will definitely make it");
+                }
+            }
+            return listForFileInform;
+        }
+    }
+
+    #endregion
+}
